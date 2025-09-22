da688b077beed5709c6968f1dc9af2978c69a9fa
Nishant Guvvada <nishant.guvvada@gmail.com>
2025-09-22 15:33:27 +0530

    read_csv

---
+++ projects/read_csv/src/main.rs
@@ -0,0 +1,19 @@
+use csv;
+use std::error::Error;
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?; // Creates a csv::Reader from the given file path.
+
+    for result in reader.records() {
+        let record = result?;
+
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = read_from_file("./reports.csv") {
+        eprintln!("{}", e);
+    }
+}
