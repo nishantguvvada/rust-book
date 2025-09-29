11d85388fec6de61f9d8905ad6a808c5de4e4dc2
Nishant Guvvada <nishant.guvvada@gmail.com>
2025-09-29 11:27:44 +0530

    added comments to get_request project

--- projects/get_request/src/main.rs
+++ projects/get_request/src/main.rs
@@ -3,8 +3,10 @@ use std::io::Read;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut res = reqwest::blocking::get("https://httpbin.org/get")?;
-    let mut body = String::new();
+    // Send a synchronous (blocking) GET request to the URL using the reqwest crate.
+    let mut body = String::new(); // Create a mutable, empty String that will hold the response body.
     res.read_to_string(&mut body)?;
+    // Read the response body into the `body` string.
 
     println!("Status: {}", res.status());
     println!("Headers: \n{:#?}", res.headers());
