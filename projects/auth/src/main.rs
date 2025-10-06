862635204bb42799febe00947e3bdd07a60a9fb2
Nishant Guvvada <nishant.guvvada@gmail.com>
2025-10-06 22:01:26 +0530

    auth updated

--- projects/auth/src/main.rs
+++ projects/auth/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+fn main() -> Result<(), Error> {
+    let client = reqwest::blocking::Client::new();
+
+    let user = String::from("testuser");
+    let passwd: Option<String> = None;
+
+    let response = client
+        .get("https://httpbin.org/get")
+        .basic_auth(user, passwd)
+        .send();
+
+    println!("{:?}", response);
+
+    Ok(())
 }
